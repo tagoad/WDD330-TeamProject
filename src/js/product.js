414f9f8f0dc4c58d02df3f921c0542c869aad3d4
tagoad <43684949+tagoad@users.noreply.github.com>
2022-03-02 21:42:00 -0600

    Initial

--- src/js/product.js
+++ src/js/product.js
@@ -1,48 +1,9 @@
-let products = [];
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+import ProductData from "./productData";
+import ProductDetails from "./productDetails";
+import { getParams } from "./utils";
 
-function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
+const datasource = new ProductData("tents");
+const productId = getParams("product");
 
-function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-// get tents data
-function getProductsData() {
-  fetch("../json/tents.json")
-    .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
-}
-// or should we do it this way?
-// async function getProductsDataAwait() {
-//   products = await fetch("../json/tents.json").then(convertToJson);
-// }
-
-// add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-
-  //Check if so-cart exists in local storage
-  if (getLocalStorage("so-cart")) {
-    const cart = getLocalStorage("so-cart");
-    cart.push(product);
-    setLocalStorage("so-cart", cart);
-  } else {
-    const cart = [product];
-    setLocalStorage("so-cart", cart);
-  }
-}
-
-getProductsData();
-// add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
+const product = new ProductDetails(productId, datasource);
+product.init();
